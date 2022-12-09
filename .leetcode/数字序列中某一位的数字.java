a8faba2e2066723085b2e36dcbed2f722f019173
wangdeguxiang <514435906@qq.com>
2022-12-09 19:15:44 +0800

    数字序列中某一位的数字

---
+++ .leetcode/数字序列中某一位的数字.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findNthDigit(int n) {
+        int len = 1;
+        while (len * 9 * Math.pow(10, len - 1) < n) {
+            n -= len * 9 * Math.pow(10, len - 1);
+            len++;
+        }
+        long s = (long) Math.pow(10, len - 1);
+        long x = n / len - 1 + s;
+        n -= (x - s + 1) * len;
+        
+        return n == 0 ? (int) (x % 10) : (int) ((x + 1) / Math.pow(10, len - n) % 10);
+    }
+}
